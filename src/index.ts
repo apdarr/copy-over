f88b51a2a264de8c73251c6d0693c4c555e95e5b
Alex Darr <alexdarr@gmail.com>
2025-05-20 13:49:43 -0400

    Clean up cruft in file

--- src/index.ts
+++ src/index.ts
@@ -26,29 +26,20 @@ const token: string = process.env.ADO_TOKEN || '';
 const authHandler = azdev.getPersonalAccessTokenHandler(token);
 const connection = new azdev.WebApi(orgUrl, authHandler);
 
-// Create a new work item
-const workItemTrackingApi = await connection.getWorkItemTrackingApi();
-const project = "ursa"; // Replace with your project name
-const workItemType = "Issue"; // Replace with your work item type
 
 export default (app: Probot) => {
   app.on("issues.opened", async (context) => {
-    console.log("Issue context is ⭐", context);
-
     // Extract issue title
     const issueTitle: string = context.payload.issue.title || "";
-    console.log("Issue title is 🔖", issueTitle);
 
     // Extract first comment
     const firstComment: string = context.payload.issue.body || "";
-    console.log("First comment is 🐸", firstComment);
     
     sendToADO(issueTitle, firstComment);
   });
 
   app.on("projects_v2_item.edited", async (context) => {
     let payload = context.payload;
-    console.log("Payload is 🐸", payload);
     
     // Extract from and to fields if they exist
     if (payload.changes && payload.changes.field_value) {
@@ -57,17 +48,9 @@ export default (app: Probot) => {
       const fromValue = changes.from || null;
       const toValue = changes.to || null;
       
-      // console.log("Field changed from:", fromValue);
-      // console.log("Field changed to:", toValue);
-      
-      // console.log("Field name is:::::::::", changes.field_name);
-      
       // Get additional metadata
       const fieldName = changes.field_name || '';
       const fieldType = changes.field_type || '';
-      // const projectNumber = changes.project_number || '';
-      
-      // console.log(`Field "${fieldName}" (${fieldType}) in project ${projectNumber} was updated`);
       
       // Check that that fromValue and toValue are not null and both exist in the STATUS_COLUMN_NAMES array
       if (fromValue && toValue && STATUS_COLUMN_NAMES.includes(fromValue.name) && STATUS_COLUMN_NAMES.includes(toValue.name)) {
@@ -78,20 +61,9 @@ export default (app: Probot) => {
       }
     }
   });
-
 }
 
-// Create a new function that sends the issueTitle and firstComment to ADO
 async function sendToADO(issueTitle: string, firstComment: string) {
-  // Construct the payload
-
-  // Use azure-devops-node-api to send the payload to ADO
-  const orgUrl = "https://dev.azure.com/ursa-minus";
-  const token: string = process.env.ADO_TOKEN || '';
-
-  const authHandler = azdev.getPersonalAccessTokenHandler(token);
-  const connection = new azdev.WebApi(orgUrl, authHandler);
-
   // Create a new work item
   const workItemTrackingApi = await connection.getWorkItemTrackingApi();
   const project = "ursa"; // Replace with your project name
@@ -147,14 +119,10 @@ async function syncStatusChange(fromValue: string, toValue: string, item: any) {
     team: "ursa Team"
   };
 
-  // "board" is the name of your board from the screenshot
-
   const boards = await workApi.getBoards(teamContext);
   console.log("Boards: 🧪", boards);
 
   const boardColumns = await workApi.getBoardColumns(teamContext, "Issues");
   console.log("Board columns: ⚗️", boardColumns);
 
-  // Example: Update the work item in ADO
-  // await updateWorkItemInADO(item, toValue);
 }
